9f908c93bdaf03e69cc826cae589588c99c5011f
Fiderana-niavo <118703864+Fiderana-niavo@users.noreply.github.com>
2022-12-08 15:09:21 +0100

    last

--- WebServer/www/Mytest.php
+++ WebServer/www/Mytest.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My first</title>
+    <title>hello</title>
 </head>
 <body>
-    <form method="post" action="Mytest.php">
-        <input type="text">
-        <input type ="submit">
-    </form>
+        <p>My exemple</p>
 </body>
 </html>
